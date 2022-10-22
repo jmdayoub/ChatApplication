65a6225cfc401e6367357e3f38fe392f64af92cd
Jared Dayoub <97490403+jmdayoub@users.noreply.github.com>
2022-10-22 12:56:16 -0700

    Add files via upload

---
+++ .Net/Models/Domain/OutgoingMessageIds.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain.Messages
+{
+    public class OutgoingMessageIds
+    {
+        public int Id { get; set; }
+        public int RecipientId { get; set; }
+    }
+}
